ba1e3977e61feddbb7a8426769ac019cb1006f4c
productivememberofsociety666 <none@example.net>
2014-08-22 03:28:27 +0200

    Changed settings so media is relative URL by default.

--- rfweb/settings.py
+++ rfweb/settings.py
@@ -57,7 +57,7 @@ RESULTS_PATH = os.path.join(MEDIA_ROOT, 'results')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://qa-master:8181/media/'
+MEDIA_URL = '/media/'
 RESULTS_URL = '/results/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
